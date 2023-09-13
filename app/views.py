e81c5b355f7311a306cdae09d771bdf070b35207
dhruba225 <dhru225@gmail.com>
2023-09-13 17:36:27 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -50,8 +50,37 @@ def display_topic(request):
     return render(request,'display_topic.html',d)
 
 def display_webpage(request):
-
+#displaying all data
     QSWO=Webpage.objects.all()
+#displaying data according to condition
+    QSWO=Webpage.objects.filter(topic_name='Cricket')
+#displaying data using exclude method
+    QSWO=Webpage.objects.exclude(topic_name='Cricket')
+#ordering data in ascending and descending order(all() is not compulsory)
+    QSWO=Webpage.objects.all().order_by('name')
+    QSWO=Webpage.objects.all().order_by('-name')
+#ordering data according to length
+    QSWO=Webpage.objects.all().order_by(Length('url'))
+    QSWO=Webpage.objects.all().order_by(Length('url').desc())
+
+
+#Field Look-ups
+#     
+#like operaters(not case-sensitive)
+    QSWO=Webpage.objects.filter(url__startswith='www')
+    QSWO=Webpage.objects.filter(url__endswith='in')
+    QSWO=Webpage.objects.filter(name__contains='r')
+#in operator
+    QSWO=Webpage.objects.filter(name__in=('Rohit','Virat'))
+#REGEx
+    QSWO=Webpage.objects.filter(url__regex=r'in$')
+
+#Q-Object-'|'
+    QSWO=Webpage.objects.filter(Q(name='Rohit') & Q(topic_name='Cricket'))
+
+#Q-Object-'&'
+    QSWO=Webpage.objects.filter(Q(name='Rohit') | Q(name='Virat'))
+    
 
     d={'QSWO':QSWO}
     return render(request,'display_webpage.html',d)
@@ -59,6 +88,7 @@ def display_webpage(request):
 def display_access_record(request):
 
     QSAO=Access_Record.objects.all()
+#greater_than
 
     d={'QSAO':QSAO}
     return render(request,'display_access_record.html',d)
